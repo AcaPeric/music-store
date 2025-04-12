b4d7ed18284dcdcc0d60f640078ac19dc3aa206e
IT30 <aleksandarwork00@gmail.com>
2025-04-12 11:35:41 +0200

    Final

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -9,20 +9,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
-  contactForm: FormGroup;
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
     this.contactForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       subject: ['', Validators.required],
-      message: ['', Validators.required]
+      message: ['', Validators.required],
     });
   }
 
-  onSubmit() {
+  onSubmit(): void {
     if (this.contactForm.valid) {
-      console.log('Form submitted:', this.contactForm.value);
+      console.log('Contact Form Submitted', this.contactForm.value);
+      alert('Message sent!');
+      this.contactForm.reset();
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,13 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
-    importProvidersFrom(ReactiveFormsModule) // 👈 add this
+    importProvidersFrom(ReactiveFormsModule, FormsModule) // 👈 add this
   ]
 });
